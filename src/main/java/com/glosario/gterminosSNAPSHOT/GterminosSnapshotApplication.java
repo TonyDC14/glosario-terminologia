a401d7ba8c3c72a5a05cbb5a6ec9d7d594e3ce7a
Anthony Castro <acastro@sms-sudamerica.com>
2023-02-02 16:04:29 -0300

    Initializer springboot project

---
+++ src/main/java/com/glosario/gterminosSNAPSHOT/GterminosSnapshotApplication.java
@@ -0,0 +1,13 @@
+package com.glosario.gterminosSNAPSHOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GterminosSnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GterminosSnapshotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glosario/gterminosSNAPSHOT/GterminosSnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glosario.gterminosSNAPSHOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GterminosSnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
